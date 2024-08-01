f57ed942d5df3bb5f07d5c325e538237b12ae9b7
김도현[ 대학원석사과정제적 / 언어학과 ] <ling_dh@korea.edu>
2024-08-01 12:03:00 -0400

    Found some difference between main, due to the merge error

--- app/Models/Deck.php
+++ app/Models/Deck.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Deck extends Model
 {
     use HasFactory;
+
+    public function user () : BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+    public function flashcards () : BelongsToMany {
+        return $this->belongsToMany(Flashcard::class);
+    }
+    public function language () : BelongsTo {
+        return $this->belongsTo(Language::class);
+    }
 }
--- app/Models/Language.php
+++ app/Models/Language.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Language extends Model
 {
     use HasFactory;
+
+    // public function users(): HasMany
+    // {
+    //     return $this->hasMany(User::class);
+    // }
+
+    public function decks(): HasMany
+    {
+        return $this->hasMany(Deck::class);
+    }
+
+    public function flashcards(): HasMany
+    {
+        return $this->hasMany(Flashcard::class);
+    }
 }

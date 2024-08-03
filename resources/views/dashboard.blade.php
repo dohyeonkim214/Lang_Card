53477b3d73b530101e086c83209758c6c5faf046
mami-mary <shio0001@algonquinlive.com>
2024-08-02 21:20:57 -0400

    added text message to User's top page, complpleted front-end

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,14 +5,14 @@
     {{ $decks }}
 
     <!-- Display User's Decks -->
-        <div class="bg-white border-b border-gray-100">
+        <div class="mt-5 bg-white border-b border-gray-100">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 font-bold h-16 text-2xl flex items-center justify-center">
                 <h2>All</h2>
             </div>
         </div>
         <div class="flex flex-wrap">
             @if($decks->isEmpty())
-            <p>You don't have any Decks yet.</p>
+            <p class="font-bold mx-auto m-5 text-lg">You don't have any Decks yet.</p>
             @else
             @foreach ($decks as $deck)
             <div class="basis-1/3 p-6">

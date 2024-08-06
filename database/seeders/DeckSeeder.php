a8226712b322f6f983ea3c5472a00c3867a95eda
mami-mary <shio0001@algonquinlive.com>
2024-08-06 17:20:57 -0400

    fix Database error

--- database/seeders/DeckSeeder.php
+++ database/seeders/DeckSeeder.php
@@ -1,22 +1,33 @@
 <?php
-namespace Database\Factories;
+namespace Database\Seeders;
 
 use App\Models\Deck;
-use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
 
-class DeckFactory extends Factory
+class DeckSeeder extends Seeder
 {
-    protected $model = Deck::class;
+    // protected $model = Deck::class;
 
-    public function definition()
+        /**
+     * Run the database seeds.
+     */
+    public function run(): void
     {
-        return [
-            'name' => $this->faker->word,
-            'completed' => $this->faker->boolean,
-            'user_id' => 1, // 또는 사용자 ID를 적절히 설정
-            'language_id' => $this->faker->numberBetween(1, 2), // 1 또는 2로 설정
-        ];
+        Deck::factory()->count(15)->create();
     }
+
+    // Same code is already defined in DeckFactory.php
+
+    // public function definition()
+    // {
+    //     return [
+    //         'name' => $this->faker->word,
+    //         'completed' => $this->faker->boolean,
+    //         'user_id' => 1, // 또는 사용자 ID를 적절히 설정
+    //         'language_id' => $this->faker->numberBetween(1, 2), // 1 또는 2로 설정
+    //     ];
+    // }
 }
 
 ?>

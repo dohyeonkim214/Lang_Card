290caf641491a0e00bc7c9d5e9b1f820422f9601
mami-mary <shio0001@algonquinlive.com>
2024-08-07 11:15:30 -0400

    styled flashcard list

--- resources/views/decks/view.blade.php
+++ resources/views/decks/view.blade.php
@@ -1,45 +1,50 @@
 <x-app-layout>
-<div class="container text-center">
-    <h1>{{ $deck->title }}</h1>
-    
+
+<div class="mt-5 bg-white border-b border-gray-100">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 font-bold h-16 text-2xl flex items-center justify-center">
+            <h2>{{ $deck->title }}</h2>
+    </div>
+</div>
+
+<div class="flex flex-wrap items-center justify-center">
     @if(session('error'))
-        <div class="alert alert-danger">
+        <div class="text-red-500 font-bold mx-auto my-5 text-lg">
             {{ session('error') }}
         </div>
     @endif
 
     @if($flashcards->count() >0)
     @foreach($flashcards as $flashcard)
-        <div class="flashcard-container flex flex-wrap justify-center">
-            <div class="basis-1/3 p-6">
-                <div class="flashcard bg-white rounded shadow m-5 p-5">
-                    <label>English</label>
-                    <p class="text-2xl">{{ $flashcard->english_text }}</p>
-                    <label>{{ $language->name}}</label>
-                    <p class="text-2xl">{{ $flashcard->second_language_text }}</p>
+        <div class="basis-1/3 p-6">
+            <div class="bg-white rounded shadow p-6">
+                <div>
+                    <label class="font-bold">English:</label>
+                    <p class="text-2xl mb-5 font-bold">{{ $flashcard->english_text }}</p>
+                    <label class="font-bold">{{ $language->name}}</label>
+                    <p class="text-2xl font-bold">{{ $flashcard->second_language_text }}</p>
                 </div>
             </div>
         </div>
-        <!-- <div class="navigation-buttons">
-            <a href="{{ route('flashcards.prev', ['deck' => $deck->id, 'flashcard' => $flashcard->id]) }}" class="btn btn-secondary">Previous</a>
-            <a href="{{ route('flashcards.next', ['deck' => $deck->id, 'flashcard' => $flashcard->id]) }}" class="btn btn-primary">Next</a>
-        </div> -->
     @endforeach
     @else
-        <h3 class="font-bold mx-auto m-5 text-lg">No flashcard yet!</h3>
-        @if (Auth::check())
-        <h4>Do you want to create Flashcards?</h4>
-        <a href="{{ route('flashcards.create', $deck) }}" class="btn btn-primary">Create Flashcard</a>
-         @endif
+    <div class="mx-auto text-center">
+        <h3 class="font-bold m-5 text-lg">No flashcard yet!</h3>
+    </div>
     @endif
+</div>
 
-    @if (Auth::id() == $deck->user_id)
-    <form class="mb-5" method="get" action="{{ route('userpage.index') }}">
-        <button class="block w-full bg-sky-400 text-white p-3 font-bold">Back to Deck List</button>
+<!-- buttons -->
+<div>
+    @if (Auth::check() && (Auth::id() == $deck->user_id))
+    <form class="m-5" method="get" action="{{ route('userpage.index') }}">
+        <button class="block w-30 bg-gray-400 text-white p-3 font-bold">< Back to Deck List</button>
+    </form>
+    <form class="m-5" method="get" action="{{ route('flashcards.create', $deck) }}">
+        <button class="block w-30 bg-sky-400 text-white p-3 font-bold">Create Flashcard</button>
     </form>
     @else
-    <form class="mb-5" method="get" action="{{ route('index') }}">
-        <button class="block w-full bg-sky-400 text-white p-3 font-bold">Back to Deck List</button>
+    <form class="m-5" method="get" action="{{ route('index') }}">
+        <button class="block w-30 bg-gray-400 text-white p-3 font-bold">< Back to Deck List</button>
     </form>
     @endif
 </div>
